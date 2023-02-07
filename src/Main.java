7011c2454268b9aa396b7d19e63f67d4ac9afcbf
João José <contato.joao.jcn@gmail.com>
2023-02-06 21:10:15 -0300

    add package

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso de java");
+        curso1.setDescricao("descrição curso de java");
+        curso1.setCargaHorario(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso de js");
+        curso2.setDescricao("descrição curso de js");
+        curso2.setCargaHorario(6);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição de mentoria de java");
+        mentoria.setDate(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
     }
 }
---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,27 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso extends Conteudo{
+    private int cargaHorario;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHorario;
+    }
+
+    public int getCargaHorario() {
+        return cargaHorario;
+    }
+
+    public void setCargaHorario(int cargaHorario) {
+        this.cargaHorario = cargaHorario;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHorario=" + cargaHorario +
+                '}';
+    }
+}
